90b02fc9e304ff90195b5f35c13892e2b25d8f69
777-Adrian-777 <81689615+777-Adrian-777@users.noreply.github.com>
2021-08-12 00:03:19 +0300

    Update conventor.cs

--- conventor.cs
+++ conventor.cs
@@ -13,20 +13,20 @@ namespace ConsoleApp1
             return fTemp;
         }
 
-        static double convertToMiles(double celLenght)
+        static double convertToMiles(double celLength)
         {
-            double fLenght = 0;
+            double fLength = 0;
 
-            fLenght = (celLenght * 15625)/25146;
+            fLength = (celLength * 15625)/25146;
 
-            return fLenght;
+            return fLength;
         }
 
         static int convertToSeconds(int celTime)
         {
             int fTime = 0;
 
-            fTime = (celTime * 1)*60;
+            fTime = (celTime * 60);
 
             return fTime;
         }
@@ -35,29 +35,29 @@ namespace ConsoleApp1
             double celTemp = 0;
             double ferTemp = 0;
 
-            double celLenght = 0;
-            double ferLenght = 0;
+            double celLength = 0;
+            double ferLength = 0;
 
             int celTime = 0;
             int ferTime = 0;
 
-            Console.WriteLine("Hello, I help you to convert some values!\n");
+            Console.WriteLine("Hello, I can help you to convert some values!\n");
             Console.Write("Enter the value of temperature in Celsius(°C): ");
             celTemp = double.Parse(Console.ReadLine());
 
             ferTemp = convertToFahrenheit(celTemp);
             Console.WriteLine("Fahrenheit temperature is(°F) : " + ferTemp);
         
-            Console.Write("\nEnter the value of lenght in kilometres: ");
-            celLenght = double.Parse(Console.ReadLine());
+            Console.Write("\nEnter the value of length in kilometres: ");
+            celLength = double.Parse(Console.ReadLine());
 
-            ferLenght = convertToMiles(celLenght);
-            Console.WriteLine("The lenght in miles is : " + ferLenght);
+            ferLength = convertToMiles(celLength);
+            Console.WriteLine("The lenght in miles is : " + ferLength);
 
             Console.Write("\nEnter the time in minutes: ");
             celTime = int.Parse(Console.ReadLine());
 
-            ferLenght = convertToSeconds(celTime);
+            ferTime = convertToSeconds(celTime);
             Console.WriteLine("The time in seconds is : " + ferTime);
         }
     }
